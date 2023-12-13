07d6ddaecca2caee38e8615b00b9bbc25f5ae772
Nathan Willett <natewillett1996@gmail.com>
2023-12-12 22:16:01 -0800

    Resolved package name conflict for chapter 3 files

--- LiangSourceCodeExamples/chapter3/SubtractionQuiz.java
+++ LiangSourceCodeExamples/chapter3/SubtractionQuiz.java
@@ -1,4 +1,4 @@
-package chapter3;
+package LiangSourceCodeExamples.chapter3;
 
 import java.util.Scanner; 
 
@@ -15,7 +15,7 @@ public class SubtractionQuiz {
       number2 = temp;
     }
 
-    // 3. Prompt the student to answer "what is number1 – number2?"
+    // 3. Prompt the student to answer "what is number1 ï¿½ number2?"
     System.out.print
       ("What is " + number1 + " - " + number2 + "? ");
     Scanner input = new Scanner(System.in);
