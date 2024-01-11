baadb188a5b613f52a1410c962ebe0f3e71434c5
Nathan Willett <natewillett1996@gmail.com>
2024-01-10 21:08:10 -0800

    ComputePay

--- CodeStepByStep/src/Small Problems 2A/ComputePay.java
+++ CodeStepByStep/src/Small Problems 2A/ComputePay.java
@@ -1,21 +1,22 @@
 public class ComputePay {
     public static void main(String[] args) {
-        int hoursWorked = 0;
-        double hourlySalary = 0.0;
-        double totalPay = 0.0;
-        double taxesOwed = 0.0;
-
         // Calculate pay at work based on hours worked each day
+        int hoursWorked = 4 + 5 + 8 + 4;
+        double hourlySalary = 8.75;
+        double totalPay = hoursWorked * hourlySalary;
+        double taxRate = 0.20;
+        double taxesOwed = totalPay * taxRate;
+
         System.out.println("My total hours worked:");
-        System.out.println(4 + 5 + 8 + 4);
+        System.out.println(hoursWorked);
 
         System.out.println("My hourly salary:");
-        System.out.println("$8.75");
+        System.out.println("$" + hourlySalary);
 
         System.out.println("My total pay:");
-        System.out.println((4 + 5 + 8 + 4) * 8.75);
+        System.out.println(totalPay);
 
         System.out.println("My taxes owed:"); // 20% tax
-        System.out.println((4 + 5 + 8 + 4) * 8.75 * 0.20);
+        System.out.println(taxesOwed);
     }
 }
