ad0f88fb20646b26e5ca02aad9be28b8c3dde4ae
Nathan Willett <natewillett1996@gmail.com>
2024-01-12 23:58:35 -0800

    starsPrint.java upload

---
+++ CodeStepByStep/src/Small Problems 2A/starsPrint_C.java
@@ -0,0 +1,11 @@
+public class starsPrint_C {
+    public static void main(String[] args) {
+        // C)
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("i");
+            }
+            System.out.println();
+        }
+    }
+}
