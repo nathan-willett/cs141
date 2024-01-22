07873a90dfe1d819cb25d9e46270fb241ac76da5
Nathan Willett <natewillett1996@gmail.com>
2024-01-22 21:06:35 +0000

    upload small problems 3b

---
+++ CodeStepByStep/src/Small_Problems_3B/circleArea.java
@@ -0,0 +1,7 @@
+/*
+ * 
+ * Write a method named circleArea that accepts the radius of a circle as a parameter (as a real number) and 
+ * returns the area of a circle with that radius. For example, the call of circleArea(2.0) should return 
+ * 12.566370614359172. You may assume that the radius passed is a non-negative number.
+ * 
+ */
