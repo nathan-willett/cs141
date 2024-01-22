298b7ec2157bc3a2db3184215ea3fb5944482707
Nathan Willett <natewillett1996@gmail.com>
2024-01-21 18:07:49 -0800

    Update numberLoops4Constant.java

--- CodeStepByStep/src/Small_Problems_3A/numberLoops4Constant.java
+++ CodeStepByStep/src/Small_Problems_3A/numberLoops4Constant.java
@@ -1,5 +1,16 @@
 package Small_Problems_3A;
 
 public class numberLoops4Constant {
-    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <=5 - i; j++
+                System.out.print(".");
+            }
+            System.out.print(i);
+            for(int j = 1; j <= i - 1; j++) {
+                System.out.print(".");
+            }
+            System.out.print();
+        }
+    }
 }
