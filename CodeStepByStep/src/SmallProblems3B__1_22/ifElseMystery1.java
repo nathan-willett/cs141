ab53b1254eec7775c6832a40c227316d76744516
Nathan Willett <natewillett1996@gmail.com>
2024-01-23 13:10:51 -0800

    completed 3B

--- CodeStepByStep/src/SmallProblems3B__1_22/ifElseMystery1.java
+++ CodeStepByStep/src/SmallProblems3B__1_22/ifElseMystery1.java
@@ -1,4 +1,7 @@
 public class ifElseMystery1 {
+    public static void main(String[] args) {
+    mystery(0);
+    }
     public static void mystery(int n) {
         System.out.print(n + " ");
         if (n > 0) {
@@ -6,7 +9,9 @@ public class ifElseMystery1 {
         }
         if (n < 0 ) {
             n = n + 7;
-            
+        } else {
+            n = n * 2;
         }
+        System.out.println(n);
     }
 }
