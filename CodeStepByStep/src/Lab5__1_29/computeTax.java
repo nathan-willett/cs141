a4c1bf6c6cfc50bca6fd04354a7288bb44a464c2
Nathan Willett <natewillett1996@gmail.com>
2024-01-24 13:55:23 -0800

    lab 4&5 start

---
+++ CodeStepByStep/src/Lab5__1_29/computeTax.java
@@ -0,0 +1,9 @@
+public class computeTax {
+    double salary = 0;
+    public static void main(String[] args) {
+
+    }
+    public static double computeTaxMethod(double salary) {
+
+    }
+}
--- CodeStepByStep/src/SmallProblems4A__1_24/RangeOfNumbers.java
+++ CodeStepByStep/src/SmallProblems4A__1_24/RangeOfNumbers.java
@@ -1,3 +1,13 @@
+/*
+ * Write a console program in a class named RangeOfNumbers that prompts the user to type two integers 
+ * and prints the sequence of numbers between the two arguments, separated by commas and spaces. Print 
+ * an increasing sequence if the first argument is smaller than the second; otherwise, print a decreasing 
+ * sequence. If the two numbers are the same, that number should be printed by itself. Here is an example 
+ * dialogue:
+ * 
+ * 
+ */
+
 public class RangeOfNumbers {
     
 }
