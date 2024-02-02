9ad65909bde4a15e322459001a40a945f6cc3676
Nathan Willett <natewillett1996@gmail.com>
2024-02-01 20:28:20 -0800

    resolved SentinelSum

--- CodeStepByStep/src/Lab7__2_4/SentinelSum.java
+++ CodeStepByStep/src/Lab7__2_4/SentinelSum.java
@@ -5,7 +5,7 @@ public class SentinelSum {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter numbers (0 to stop): ");
         
-        int 
+        //int 
         while(console != -1) {
             
         }
