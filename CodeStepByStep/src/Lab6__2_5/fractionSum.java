a2b63df2050c91af0fe36d1c82b1312d3cd9c5b3
nathan-willett <natewillett1996@gmail.com>
2024-02-04 02:36:51 +0000

    fractionSum

--- CodeStepByStep/src/Lab6__2_5/fractionSum.java
+++ CodeStepByStep/src/Lab6__2_5/fractionSum.java
@@ -1,5 +1,10 @@
 package Lab6__2_5;
 
-public class fractionSum {
-    
+public static double fractionSum(int n) {
+    int denominator = 1;
+    double sum = 0.0;
+    for(int i = 0; i <= n; i++) {
+        sum += (double) 1 / denominator;
+    }
+    return  sum;
 }
