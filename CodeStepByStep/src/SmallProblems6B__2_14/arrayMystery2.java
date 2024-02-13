7c239fef338b8bccc3fcbf5488754016c353fb65
Nathan Willett <natewillett1996@gmail.com>
2024-02-13 13:20:07 -0800

    arraymystery2

--- CodeStepByStep/src/SmallProblems6B__2_14/arrayMystery2.java
+++ CodeStepByStep/src/SmallProblems6B__2_14/arrayMystery2.java
@@ -1,18 +1,26 @@
 /*
  * Consider the following method:
  * 
- * For each array below, indicate what the array's contents would be after 
- * the method mystery were called and passed that array as its parameter.
+ * For each array below, indicate what the array's contents would be after the method mystery 
+ * were called and passed that array as its parameter.
  */
 
 package SmallProblems6B__2_14;
 
 public class arrayMystery2 {
     public static void main(String[]args) {
-
+        int[] list = {6, 3};
+        System.out.println(mystery2(list));
     }
 
-    public static void mystery2(int[] list) {
-
+    public static int mystery2(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            if (i % 2 == 0) {
+                list[i]++;
+            } else {
+                list[i]--;
+            }
+        }
+        return list;
     }
 }
