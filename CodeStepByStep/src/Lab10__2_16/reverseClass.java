e596efce2e8baf62975b33ada28868a412eee497
Nathan Willett <natewillett1996@gmail.com>
2024-02-15 23:01:48 -0800

    lab 10

--- CodeStepByStep/src/Lab10__2_16/reverseClass.java
+++ CodeStepByStep/src/Lab10__2_16/reverseClass.java
@@ -1,5 +1,17 @@
+/*
+ * Write a method named reverse that accepts an array of integers as a parameter and modifies the array to be
+ * in reverse order. For example, if the array a stores {10, -2, 33, 55, 9}, the call of reverse(a) should 
+ * modify it to store {9, 55, 33, -2, 10}.
+ */
+
 package Lab10__2_16;
 
 public class reverseClass {
-    
+    public static void main(String[] args) {
+        int[] array = {10, -2, 33, 55, 9};
+    }
+
+    public static void reverse() {
+        
+    }
 }
