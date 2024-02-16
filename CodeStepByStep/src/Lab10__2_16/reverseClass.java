1def64cebcbf4f09a7904263ff7bb61654211876
Nathan Willett <natewillett1996@gmail.com>
2024-02-16 13:17:08 -0800

    lab 10 + 6B

--- CodeStepByStep/src/Lab10__2_16/reverseClass.java
+++ CodeStepByStep/src/Lab10__2_16/reverseClass.java
@@ -6,12 +6,21 @@
 
 package Lab10__2_16;
 
+import java.util.Arrays;
+
 public class reverseClass {
     public static void main(String[] args) {
         int[] array = {10, -2, 33, 55, 9};
+        reverse(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static void reverse() {
-        
+    public static void reverse(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[length - i - 1];
+            array[length - i - 1] = temp;
+        }
     }
 }
