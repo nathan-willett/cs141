ca95e003f27dfe6b2d9e77302fc9240f2d114a13
nathan-willett <natewillett1996@gmail.com>
2024-02-17 02:00:09 +0000

    6b + ch 7

---
+++ LiangSourceCodeExamples/src/chapter7/RandomShuffling.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+import java.util.Arrays;
+
+public class RandomShuffling {
+  public static void main(String[] args) {
+    double[] myList = {1.9, 2.9, 3.4, 3.5};
+    RandomShuffle(myList);
+    System.out.println(Arrays.toString())
+  }
+
+  public static void RandomShuffle(int[] myList) {
+    for (int i = array.length - 1; i >= 0; i--) {
+  }
+}
