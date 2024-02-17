b8af7fbb4f061339a40bc26f576b7565700b486c
nathan-willett <natewillett1996@gmail.com>
2024-02-17 04:14:48 +0000

    fixed random shuffling

--- LiangSourceCodeExamples/src/chapter7/RandomShuffling.java
+++ LiangSourceCodeExamples/src/chapter7/RandomShuffling.java
@@ -6,10 +6,18 @@ public class RandomShuffling {
   public static void main(String[] args) {
     double[] myList = {1.9, 2.9, 3.4, 3.5};
     RandomShuffle(myList);
-    System.out.println(Arrays.toString())
+    System.out.println(Arrays.toString(myList));
   }
 
-  public static void RandomShuffle(int[] myList) {
-    for (int i = array.length - 1; i >= 0; i--) {
+  public static void RandomShuffle(double[] myList) {
+    for (int i = 0; i < myList.length; i++) {
+      // Generate an index j randomly
+      int j = (int)(Math.random() * myList.length);
+
+      // Swap myList[i] with myList[j]
+      double temp = myList[i];
+      myList[i] = myList[j];
+      myList[j] = temp;
+    }
   }
 }
