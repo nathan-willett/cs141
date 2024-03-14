ac4697197a79fd9917a8b00e39e3ade2b8dbfc9e
Nathan Willett <natewillett1996@gmail.com>
2024-03-14 01:19:16 -0700

    upload remaining lab1

--- CodeStepByStep/src/Quiz7__2_26/arrayMysteryExam5.java
+++ CodeStepByStep/src/Quiz7__2_26/arrayMysteryExam5.java
@@ -1,5 +1,8 @@
 package Quiz7__2_26;
 
 public class arrayMysteryExam5 {
-    
+    public static void main(String[] args) {
+        int[] a1 = {2, 4};
+        System.out.println(arrayMystery(a1));
+    }
 }
